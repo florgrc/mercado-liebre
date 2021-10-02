a4d92a0a33e41ca12111958a058bf0225dfb1b91
florgrc <florencia.grc@hotmail.com>
2021-10-02 19:54:57 -0300

    Mercado-liebre-FG

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ("express")
+const app = express()
+const path = require ("path")
+
+app.use(express.static(path.resolve(__dirname,"public")));
+
+app.get("/", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+})
+app.get("/register", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+})
+app.post("/register", (req,res) => {
+    res.send("Te registraste con exito!");
+})
+app.get("/login", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+})
+
+app.listen(process.env.PORT || 3000,() => console.log("Server corriendo en el puerto 3000"));
